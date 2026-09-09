0e4a6a48c98680695c41ecaf488464bd6f6c1547
AtesiT <atesit@mail.ru>
2026-09-09 23:56:42 +0300

    ADD: systemIcon for menu bar

--- MacStats/MacStatsApp.swift
+++ MacStats/MacStatsApp.swift
@@ -12,7 +12,19 @@ struct MacStatsApp: App {
 }
 
 class AppDelegate: NSObject, NSApplicationDelegate {
+    var statusItem: NSStatusItem!
+
     func applicationDidFinishLaunching(_ notification: Notification) {
-        print("app launched")
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+
+        if let button = statusItem.button {
+            button.image = NSImage(systemSymbolName: "cpu", accessibilityDescription: "MacStats")
+            button.action = #selector(togglePopover)
+            button.target = self
+        }
+    }
+
+    @objc func togglePopover() {
+        print("clicked")
     }
 }

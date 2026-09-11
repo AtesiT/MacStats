7f480a28a83fa0e6817cba37713db9d908276d1d
AtesiT <atesit@mail.ru>
2026-09-11 18:13:57 +0300

    ADD: func to get a CPU Usage per core

--- MacStats/ContentView.swift
+++ MacStats/ContentView.swift
@@ -148,3 +148,31 @@ func getDiskSpace() -> (free: Double, total: Double) {
 
     return (free / 1_073_741_824, total / 1_073_741_824)
 }
+
+func getCPUUsagePerCore() -> [Double] {
+    var numCPUsU: natural_t = 0
+    var cpuInfo: processor_info_array_t!
+    var numCpuInfo: mach_msg_type_number_t = 0
+
+    let result = host_processor_info(mach_host_self(), PROCESSOR_CPU_LOAD_INFO, &numCPUsU, &cpuInfo, &numCpuInfo)
+    if result != KERN_SUCCESS {
+        return []
+    }
+
+    var usages: [Double] = []
+
+    for i in 0..<Int(numCPUsU) {
+        let user = Double(cpuInfo[Int(CPU_STATE_MAX) * i + Int(CPU_STATE_USER)])
+        let sys = Double(cpuInfo[Int(CPU_STATE_MAX) * i + Int(CPU_STATE_SYSTEM)])
+        let idle = Double(cpuInfo[Int(CPU_STATE_MAX) * i + Int(CPU_STATE_IDLE)])
+        let nice = Double(cpuInfo[Int(CPU_STATE_MAX) * i + Int(CPU_STATE_NICE)])
+
+        let total = user + sys + idle + nice
+        usages.append(total > 0 ? (user + sys + nice) / total * 100 : 0)
+    }
+
+    let size = vm_size_t(numCpuInfo) * vm_size_t(MemoryLayout<integer_t>.stride)
+    vm_deallocate(mach_task_self_, vm_address_t(bitPattern: cpuInfo), size)
+
+    return usages
+}
